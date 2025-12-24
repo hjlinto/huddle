871b40b48886e198d3c8f7582a4ac567e5691b2e
Hunter Linton <hlinto2@wgu.edu>
2025-12-23 23:30:16 -0500

    Add odds table and loaders for games and spread/total snapshots

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
-from datetime import date
+from datetime import date, datetime
 from .extensions import db
 
 class Game(db.Model):
-    __fsa__lename__ = 'games'
+    __tablename__ = 'games'
     id = db.Column(db.Integer, primary_key=True)
     season = db.Column(db.Integer, nullable=False)
     week = db.Column(db.Integer, nullable=False)
@@ -15,3 +15,17 @@ class Game(db.Model):
     away_score = db.Column(db.Integer, nullable=True)
 
     home_team_wins = db.Column(db.Boolean, nullable=True)
+
+    is_final = db.Column(db.Boolean, nullable=False, default=False)
+
+class Odds(db.Model):
+    __tablename__ = 'odds'
+    id = db.Column(db.Integer, primary_key=True)
+    game_id = db.Column(db.Integer, db.ForeignKey('games.id'), nullable=False, unique=True)
+    spread = db.Column(db.Float, nullable=True)
+    total = db.Column(db.Float, nullable=True)
+    
+
+    snapshot_ts = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    
+    game = db.relationship('Game', backref=db.backref('odds', uselist=False))
